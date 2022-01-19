f61992d2620d736d3312dae6e2cf784e9907adcb
Maaz Ahmed <93466155+CodewithMaaz@users.noreply.github.com>
2022-01-19 14:01:48 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# Area of cube
+a = input("Enter Edge ")
+a = int(a)
+
+
+area = 6 * a * a
+text = 'Your Area is %s'
+
+print( text % area )
+
+from colorama import Fore, Back, Style
+print(Fore.GREEN + 'Made By code with maaz')
+print(Fore.BLUE + 'Cube calculator')
+print(Fore.RED + 'Thank you for using me')
+print(Style.RESET_ALL)
+
